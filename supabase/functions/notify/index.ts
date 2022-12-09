713267844985b8200216a11f480f11345d390b60
dshukertjr <18113850+dshukertjr@users.noreply.github.com>
2022-12-09 22:39:06 +0900

    simplifying the function

--- supabase/functions/notify/index.ts
+++ supabase/functions/notify/index.ts
@@ -1,24 +1,27 @@
-import { serve } from "https://deno.land/std@0.131.0/http/server.ts"
-import * as OneSignal from "https://esm.sh/@onesignal/node-onesignal@1.0.0-beta7"
-import { onesignal, _OnesignalAppId_ } from "../_utils/config.ts"
-import { getCustomerProfile } from "../_utils/supabase.ts"
+import { serve } from 'https://deno.land/std@0.131.0/http/server.ts'
+import * as OneSignal from 'https://esm.sh/@onesignal/node-onesignal@1.0.0-beta7'
 
 const generateMessage = (amount: number, currency: string) =>
   `You just spent ${amount / 100} ${(currency as String).toUpperCase()}.`
 
+const _OnesignalAppId_ = Deno.env.get('ONESIGNAL_APP_ID')!
+const _OnesignalUserAuthKey_ = Deno.env.get('USER_AUTH_KEY')!
+const _OnesignalRestApiKey_ = Deno.env.get('ONESIGNAL_REST_API_KEY')!
+const configuration = OneSignal.createConfiguration({
+  userKey: _OnesignalUserAuthKey_,
+  appKey: _OnesignalRestApiKey_,
+})
+
+const onesignal = new OneSignal.DefaultApi(configuration)
+
 serve(async (req) => {
   try {
     const { record } = await req.json()
-    const customerId = record.stripe_customer_id
-    const profile = await getCustomerProfile(customerId)
-    if (!profile) {
-      throw Error(`No profile found for Stripe customer ${customerId}.`)
-    }
 
     // Build OneSignal notification object
     const notification = new OneSignal.Notification()
     notification.app_id = _OnesignalAppId_
-    notification.include_external_user_ids = [profile.id]
+    notification.include_external_user_ids = [record.user_id]
     notification.contents = {
       en: generateMessage(record.amount, record.currency),
     }
@@ -27,13 +30,13 @@ serve(async (req) => {
     return new Response(
       JSON.stringify({ onesignalResponse: onesignalApiRes }),
       {
-        headers: { "Content-Type": "application/json" },
+        headers: { 'Content-Type': 'application/json' },
       }
     )
   } catch (err) {
-    console.error("Failed to create OneSignal notification", err)
-    return new Response("Server error.", {
-      headers: { "Content-Type": "application/json" },
+    console.error('Failed to create OneSignal notification', err)
+    return new Response('Server error.', {
+      headers: { 'Content-Type': 'application/json' },
       status: 400,
     })
   }
